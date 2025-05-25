fb46749fae703bbe06cc7b2157a630e71ffb1054
Dimitri MOMPEROUSSE <momperousse.dimitri.pro@gmail.com>
2025-05-26 01:22:54 +0200

    first commit

---
+++ droneAppSpaceXBack/src/main/java/com/example/droneAppSpaceXBack/DroneAppSpaceXBackApplication.java
@@ -0,0 +1,13 @@
+package com.example.droneAppSpaceXBack;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DroneAppSpaceXBackApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DroneAppSpaceXBackApplication.class, args);
+	}
+
+}
---
+++ droneAppSpaceXBack/src/test/java/com/example/droneAppSpaceXBack/DroneAppSpaceXBackApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.droneAppSpaceXBack;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DroneAppSpaceXBackApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
